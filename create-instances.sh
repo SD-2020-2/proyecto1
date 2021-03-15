0525bc1a04617d87660b14ef2736f2b0c4332a81
augusticor <rojascruzoscar@gmail.com>
2021-03-15 17:35:59 -0500

    Simple logging v1

--- create-instances.sh
+++ create-instances.sh
@@ -1,9 +1,17 @@
+echo '... Analizando instancias ...'
 docker stop $(docker ps -q --filter ancestor=instance) | xargs docker rm
 
 docker rmi instance
 
+echo ''
+echo '... Construyendo imagen ...'
 docker build -t instance .
 
+echo ''
+echo '... Iniciando instancias ...'
 docker run --name instance1 -p 4000:4000 -d instance
 docker run --name instance2 -p 4001:4000 -d instance
 docker run --name instance3 -p 4002:4000 -d instance
+
+# Ver las ips de los contenedores
+#docker inspect -f '{{.Name}} {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -q)
