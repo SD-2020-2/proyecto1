32510e03bd81382511d8437e5da299eac673600d
augusticor <rojascruzoscar@gmail.com>
2021-03-18 08:19:33 -0500

    Logging funcionando con Loki
    - Logging con Loki
    - Logs en las instancias con Winston
    - Se guardan en archivo instance.log, este no se sube al repo
    - Se muestran estos logs en grafana

--- instance/app.js
+++ instance/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const morgan = require('morgan');
+const logger = require('./logging/logger');
 
 const server = express();
 const port = 4000;
+
 server.use(express.text({ limit: '60mb' }));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(morgan('common'));
 
 server.use(require('./routes/index.routes'));
 
 server.listen(port, () => {
-	console.log(`Instancia corriendo en ${port}`);
+	// console.log(`Instancia corriendo en ${port}`);
+	logger.log('alert', `Instancia corriendo en ${port}`);
 });
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const morgan = require('morgan');
 
 const server = express();
 const port = 2000;
@@ -7,7 +6,6 @@ const port = 2000;
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
-server.use(morgan('common'));
 server.use(require('./routes/index.routes'));
 
 const axios = require('axios');
